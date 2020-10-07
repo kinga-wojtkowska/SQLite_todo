fb3155345f4c4f85425e771ed825ec2cd8ba5298
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-10-07 22:46:05 +0200

    fix problem with db

--- sql_todo.py
+++ sql_todo.py
@@ -1,11 +1,9 @@
+from flask import Flask
 from todo_app.views import app
 import sqlite3
 import os
 
 app.config["SECRET_KEY"] = "nininini"
-
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-db_file = os.path.join(THIS_FOLDER, 'to_do.db')
 
 
 def create_connection(db_file):
@@ -15,7 +13,6 @@ def create_connection(db_file):
 
 if __name__ == "__main__":
 
-    app.run(debug=True)
     db_file = "todo_app/to_do.db"
 
     con = create_connection(db_file)
@@ -30,7 +27,9 @@ if __name__ == "__main__":
         )""")
 
     cur.execute('INSERT INTO todo VALUES(NULL, ?, ?, ?);', ('Sport', '30-minutowy spacer', 0))  # noqa: E501
-    cur.execute('INSERT INTO todo VALUES(NULL, ?, ?, ?);', ('cyk', '30-minutowy spacer', 0))  # noqa: E501
-    cur.execute('INSERT INTO todo VALUES(NULL, ?, ?, ?);', ('myk', '30-minutowy spacer', 0))  # noqa: E501
-    cur.execute('INSERT INTO todo VALUES(NULL, ?, ?, ?);', ('fik', '30-minutowy spacer', 0))  # noqa: E501
+    cur.execute('INSERT INTO todo VALUES(NULL, ?, ?, ?);', ('cyk', 'biegi', 0))  # noqa: E501
+    cur.execute('INSERT INTO todo VALUES(NULL, ?, ?, ?);', ('myk', 'rowerek', 0))  # noqa: E501
+    cur.execute('INSERT INTO todo VALUES(NULL, ?, ?, ?);', ('fik', 'odpoczynek tez musi byc', 0))  # noqa: E501
     con.commit()
+
+    exit()
